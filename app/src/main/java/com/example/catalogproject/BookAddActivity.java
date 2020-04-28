45921edc3d208c3689d41d8346d35af1a11b96ea
Ben Civjan <bcivjan2@illinois.edu>
2020-04-28 14:17:57 -0700

    Added spinner

--- app/src/main/java/com/example/catalogproject/BookAddActivity.java
+++ app/src/main/java/com/example/catalogproject/BookAddActivity.java
@@ -6,8 +6,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.catalogproject.Logic.Book;
@@ -20,7 +24,8 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class BookAddActivity extends AppCompatActivity {
+public class BookAddActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private String genre;
     private RemoteMongoCollection mongoCollection = MainActivity.getMongoCollection();
     private RemoteMongoClient mongoClient = MainActivity.getMongoClient();
     @Override
@@ -30,13 +35,20 @@ public class BookAddActivity extends AppCompatActivity {
 
         EditText titleEditText = findViewById(R.id.titleEditText);
         EditText authorEditText = findViewById(R.id.authorEditText);
-        EditText genreEditText = findViewById(R.id.genreEditText);
         EditText descEditText = findViewById(R.id.descriptionEditText);
         Button addBookButton = findViewById(R.id.addBookButton);
+        Spinner spinner = (Spinner) findViewById(R.id.genreSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.genre_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
         addBookButton.setOnClickListener(v -> {
             String title = titleEditText.getText().toString();
             String author = authorEditText.getText().toString();
-            String genre = genreEditText.getText().toString().toLowerCase();
+            String selectedGenre = genre;
             String description = descEditText.getText().toString();
             // Would normally send add request to MongoDB here
 
@@ -60,5 +72,15 @@ public class BookAddActivity extends AppCompatActivity {
             }
             // End new book test
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        genre = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //Do nothing
     }
 }
