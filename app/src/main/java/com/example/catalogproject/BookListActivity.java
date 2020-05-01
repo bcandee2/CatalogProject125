5ef6fe6379112a8bcba57be45c878ae10f2ebb2c
bcandee2 <bcandee2@illinois.edu>
2020-04-30 22:38:31 -0500

    Edited updateBooksUi to list sortedBooks instead of books

--- app/src/main/java/com/example/catalogproject/BookListActivity.java
+++ app/src/main/java/com/example/catalogproject/BookListActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.catalogproject.Logic.Book;
@@ -16,10 +19,12 @@ import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
 
 import java.util.ArrayList;
 
-public class BookListActivity extends AppCompatActivity {
+public class BookListActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private final RemoteMongoCollection mongoCollection = MainActivity.getMongoCollection();
     private final RemoteMongoClient mongoClient = MainActivity.getMongoClient();
     private ArrayList<Book> books;
+    private ArrayList<Book> sortedBooks;
+    private String sortField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +33,31 @@ public class BookListActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         books = (ArrayList<Book>) bundle.getSerializable("books");
+        sortedBooks = Book.sortBy(books, "title");
 
         updateBooksUi();
+
+        Spinner sortSpinner = findViewById(R.id.sortSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sort_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        sortSpinner.setAdapter(adapter);
+        sortSpinner.setOnItemSelectedListener(this);
+        Button sortButton = findViewById(R.id.sortButton);
+        sortButton.setOnClickListener(v -> {
+            sortedBooks = Book.sortBy(books, sortField);
+            updateBooksUi();
+        });
+
     }
 
     protected void updateBooksUi() {
         LinearLayout booksLayout = findViewById(R.id.listLayout);
         booksLayout.removeAllViews();
-        for (Book b: books) {
+        for (Book b: sortedBooks) {
             View chunk = getLayoutInflater().inflate(R.layout.chunk_addedbook, booksLayout, false);
             TextView text = chunk.findViewById(R.id.bookInfoText);
             text.setText(b.toString());
@@ -52,9 +74,19 @@ public class BookListActivity extends AppCompatActivity {
             Button removeButton = chunk.findViewById(R.id.bookRemoveButton);
             removeButton.setOnClickListener(v -> {
                 books.remove(b);
+                sortedBooks.remove(b);
                 updateBooksUi();
             });
             booksLayout.addView(chunk);
         }
     }
-}
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        sortField = parent.getItemAtPosition(position).toString().toLowerCase();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        sortField = "title";
+    }}
